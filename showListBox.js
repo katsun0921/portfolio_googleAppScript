493f1a2cf6091a6aed7b8b253ef7523c631dd6e9
Katsumasa-Sato <katsumasa-sato@outlook.com>
2022-02-19 16:32:55 +0900

    Fix check to input tag from select tag

--- showListBox.js
+++ showListBox.js
@@ -1,31 +1,3 @@
-const FUNC_NAME = "showListBox";
-
-function init() {
-  try{
-    // 現在登録されているトリガー一覧を取得
-    let triggers = ScriptApp.getProjectTriggers();
-    // FUNC_NAME という関数がトリガーに登録されているかチェック
-    let target = triggers.findIndex(trigger => trigger.getHandlerFunction() === FUNC_NAME);
-
-    if(target !== -1) {
-      Browser.msgBox('It has already initialized.');
-      return 0;
-    }
-
-    let spreadsheet = SpreadsheetApp.getActive();
-    // トリガーを設定
-    ScriptApp.newTrigger(FUNC_NAME)
-    .forSpreadsheet(spreadsheet)
-    .onEdit()
-    .create();
-
-  }catch(e){
-    // エラー
-    Logger.log(e);
-    Browser.msgBox('Initialization failed: ' + e);
-  }
-}
-
 function doGet(fileName) {
   return HtmlService.createTemplateFromFile(fileName).evaluate();
 }
